b80c10dd3c7b065363e50559c0646107ed2a46cb
JiaoFusen <jiao19921010@163.com>
2016-12-22 19:44:07 +0800

    database and some web page done

--- myonlineshop/homepage/models.py
+++ myonlineshop/homepage/models.py
@@ -1,3 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
+
+class Admin(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+
+
+class Category(models.Model):
+    c_id = models.AutoField(primary_key=True,)
+    name = models.CharField(max_length=20)
+
+
+class Details(models.Model):
+    detailId = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    price = models.FloatField()
+    remains = models.IntegerField()
+    memo = models.CharField(max_length=200)
+    c_id = models.IntegerField()
+
+
+class Income(models.Model):
+    detailId = models.CharField(max_length=50)
+    number = models.IntegerField()
+    date = models.DateField()
+    memo = models.CharField(max_length=200)
+
+
+class Outcome(models.Model):
+    detailId = models.CharField(max_length=50)
+    number = models.IntegerField()
+    date = models.DateField()
+    memo = models.CharField(max_length=200)
+
 # Create your models here.
+
--- myonlineshop/myonlineshop/settings.py
+++ myonlineshop/myonlineshop/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'myonlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tzguanzhu',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306'
+
     }
 }
 
